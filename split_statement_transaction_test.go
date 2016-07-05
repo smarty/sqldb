df702170886106cebdf6126584603f436ecf7035
Oliver, Jonathan <git@jonathanoliver.com>
2016-07-05 11:40:59 -0600

    Renamed variable.

--- split_statement_transaction_test.go
+++ split_statement_transaction_test.go
@@ -10,55 +10,55 @@ import (
 type SplitStatementTransactionFixture struct {
 	*gunit.Fixture
 
-	fakeInner   *FakeInnerTransaction
+	inner       *FakeInnerTransaction
 	transaction *SplitStatementTransaction
 }
 
 func (this *SplitStatementTransactionFixture) Setup() {
-	this.fakeInner = &FakeInnerTransaction{}
-	this.transaction = NewSplitStatementTransaction(this.fakeInner, "?")
+	this.inner = &FakeInnerTransaction{}
+	this.transaction = NewSplitStatementTransaction(this.inner, "?")
 }
 
 ///////////////////////////////////////////////////////////////
 
 func (this *SplitStatementTransactionFixture) TestCommit() {
-	this.fakeInner.commitError = errors.New("")
+	this.inner.commitError = errors.New("")
 
 	err := this.transaction.Commit()
 
-	this.So(err, should.Equal, this.fakeInner.commitError)
-	this.So(this.fakeInner.commitCalls, should.Equal, 1)
+	this.So(err, should.Equal, this.inner.commitError)
+	this.So(this.inner.commitCalls, should.Equal, 1)
 }
 
 func (this *SplitStatementTransactionFixture) TestRollback() {
-	this.fakeInner.rollbackError = errors.New("")
+	this.inner.rollbackError = errors.New("")
 
 	err := this.transaction.Rollback()
 
-	this.So(err, should.Equal, this.fakeInner.rollbackError)
-	this.So(this.fakeInner.rollbackCalls, should.Equal, 1)
+	this.So(err, should.Equal, this.inner.rollbackError)
+	this.So(this.inner.rollbackCalls, should.Equal, 1)
 }
 
 func (this *SplitStatementTransactionFixture) TestSelect() {
-	this.fakeInner.selectError = errors.New("")
-	this.fakeInner.selectResult = &FakeSelectResult{}
+	this.inner.selectError = errors.New("")
+	this.inner.selectResult = &FakeSelectResult{}
 
 	result, err := this.transaction.Select("query", 1, 2, 3)
 
-	this.So(result, should.Equal, this.fakeInner.selectResult)
-	this.So(err, should.Equal, this.fakeInner.selectError)
-	this.So(this.fakeInner.selectCalls, should.Equal, 1)
-	this.So(this.fakeInner.selectStatement, should.Equal, "query")
-	this.So(this.fakeInner.selectParameters, should.Resemble, []interface{}{1, 2, 3})
+	this.So(result, should.Equal, this.inner.selectResult)
+	this.So(err, should.Equal, this.inner.selectError)
+	this.So(this.inner.selectCalls, should.Equal, 1)
+	this.So(this.inner.selectStatement, should.Equal, "query")
+	this.So(this.inner.selectParameters, should.Resemble, []interface{}{1, 2, 3})
 }
 
 func (this *SplitStatementTransactionFixture) TestExecute() {
-	this.fakeInner.executeResult = 5
+	this.inner.executeResult = 5
 
 	affected, err := this.transaction.Execute("statement1 ?; statement2 ? ?;", 1, 2, 3)
 
 	this.So(affected, should.Equal, 10)
 	this.So(err, should.BeNil)
-	this.So(this.fakeInner.executeCalls, should.Equal, 2)
-	this.So(this.fakeInner.executeParameters, should.Resemble, []interface{}{2, 3}) // last two parameters
+	this.So(this.inner.executeCalls, should.Equal, 2)
+	this.So(this.inner.executeParameters, should.Resemble, []interface{}{2, 3}) // last two parameters
 }
