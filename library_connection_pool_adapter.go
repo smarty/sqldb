8a485a4b99cbad4603aef5dfaa085841254aaae4
Mike Carpenter <mike.carpenter@smarty.com>
2025-07-22 13:58:04 -0600

    Initial add of prepared statements

--- library_connection_pool_adapter.go
+++ library_connection_pool_adapter.go
@@ -7,7 +7,8 @@ import (
 
 type LibraryConnectionPoolAdapter struct {
 	*sql.DB
-	txOptions *sql.TxOptions
+	txOptions  *sql.TxOptions
+	statements map[string]*sql.Stmt
 }
 
 func NewLibraryConnectionPoolAdapter(actual *sql.DB, txOptions *sql.TxOptions) *LibraryConnectionPoolAdapter {
@@ -26,6 +27,26 @@ func (this *LibraryConnectionPoolAdapter) BeginTransaction(ctx context.Context) 
 	}
 }
 
+func (this *LibraryConnectionPoolAdapter) ExecuteStatement(ctx context.Context, id, statement string, parameters ...any) (uint64, error) {
+	preparedStatement, ok := this.statements[id]
+	if !ok {
+		preparedStatement, err := this.DB.PrepareContext(ctx, statement)
+		if err != nil {
+			return 0, err
+		}
+
+		this.statements[id] = preparedStatement
+	}
+
+	result, err := preparedStatement.ExecContext(ctx, parameters...)
+	if err != nil {
+		return 0, err
+	} else {
+		count, _ := result.RowsAffected()
+		return uint64(count), nil
+	}
+}
+
 func (this *LibraryConnectionPoolAdapter) Execute(ctx context.Context, query string, parameters ...any) (uint64, error) {
 	if result, err := this.DB.ExecContext(ctx, query, parameters...); err != nil {
 		return 0, err
@@ -37,4 +58,12 @@ func (this *LibraryConnectionPoolAdapter) Execute(ctx context.Context, query str
 
 func (this *LibraryConnectionPoolAdapter) Select(ctx context.Context, query string, parameters ...any) (SelectResult, error) {
 	return this.DB.QueryContext(ctx, query, parameters...)
+}
+
+func (this *LibraryConnectionPoolAdapter) CloseStatement(id string) {
+	preparedStatement, ok := this.statements[id]
+	if ok {
+		_ = preparedStatement.Close()
+		delete(this.statements, id)
+	}
 }
--- library_transaction_adapter.go
+++ library_transaction_adapter.go
@@ -7,10 +7,31 @@ import (
 
 type LibraryTransactionAdapter struct {
 	*sql.Tx
+	statements map[string]*sql.Stmt
 }
 
 func NewLibraryTransactionAdapter(actual *sql.Tx) *LibraryTransactionAdapter {
 	return &LibraryTransactionAdapter{Tx: actual}
+}
+
+func (this *LibraryTransactionAdapter) ExecuteStatement(ctx context.Context, id, statement string, parameters ...any) (uint64, error) {
+	preparedStatement, ok := this.statements[id]
+	if !ok {
+		preparedStatement, err := this.Tx.PrepareContext(ctx, statement)
+		if err != nil {
+			return 0, err
+		}
+
+		this.statements[id] = preparedStatement
+	}
+
+	result, err := preparedStatement.ExecContext(ctx, parameters...)
+	if err != nil {
+		return 0, err
+	} else {
+		count, _ := result.RowsAffected()
+		return uint64(count), nil
+	}
 }
 
 func (this *LibraryTransactionAdapter) Execute(ctx context.Context, query string, parameters ...any) (uint64, error) {
@@ -24,4 +45,12 @@ func (this *LibraryTransactionAdapter) Execute(ctx context.Context, query string
 
 func (this *LibraryTransactionAdapter) Select(ctx context.Context, query string, parameters ...any) (SelectResult, error) {
 	return this.Tx.QueryContext(ctx, query, parameters...)
+}
+
+func (this *LibraryTransactionAdapter) CloseStatement(id string) {
+	preparedStatement, ok := this.statements[id]
+	if ok {
+		_ = preparedStatement.Close()
+		delete(this.statements, id)
+	}
 }
